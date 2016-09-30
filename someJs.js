ce28210e4f4765ba1bc80d005b26bffb73ef574b
ZeFlow <toni.no@live.no>
2016-09-30 16:24:08 +0200

    first commit in for loop way
    *Made init button act like it should
    *made update display function work
    *added error msg
    *fa***up the css a bit
    *more comments!

--- someJs.js
+++ someJs.js
@@ -5,50 +5,53 @@ window.onload = function(){
 	let initB = document.getElementById("initB");
 	let makeB = document.getElementById("makeB");
 	let resultS = document.getElementById("resultS");
-	let statesA = ["notReady","pending..","pending...","ready!","done!"];
+	let statesA = ["Not Ready","pending..","pending...","Ready!","Done!","!3rr0r"];
 	
 	window.console.log('some vars are statesA:'+statesA+'initB:'+initB+'makeB:'+makeB+'result:'+resultS);
 	
 	var inx1 = 0;
-	var inx2 =0;
+	var inx2 = 0;
 	inx2 = statesA[inx1];
 
 	window.console.log("No buttons pushed. inx1 ="+inx1+" and inx2 = "+inx2);
 	
+	//defines the function that updates the status display
 	function updateDisplayF(){
 		
-		let disp = document.getElementById("resultDiv")
 		inx2 = statesA[inx1];
-		disp = inx2;
-		window.console.log("update display runs. inx1 ="+inx1+" and inx2 = "+disp);
+		document.getElementById("resultS").innerHTML = inx2;
+		
+		
+		window.console.log("update display runs. inx1 ="+inx1+" and inx2 = "+inx2);
 	};
-	
+	//defines what happens when pressing Initilize button
 	function initilizeF () {
 		
-		inx1+=1;
+		//update to know sht is going down
+		inx1=1;
 		updateDisplayF();
 		
+		//cycletrough yet non existent checkposts
 		for (i=0; i<2; i++) {
+			
+			//delay for the illusion of heavy program lol
 			setTimeout(function(){inx1+=1;updateDisplayF();},5500);
 			
 			
 		};
 		
 	};	
-	
-	
-	function makeCoffeeF (statesA) {
-		if (inx1==3){
-		setTimeout(function(statesA){statesA = "done";},5500);
-		window.console.log(statesA);
-		return statesA;
-		}
+	//defines what happens when pressing Make button
+	function makeCoffeeF () {
+		window.alert('no! don\'t press this! the function is yet under construction!');
+		inx1 = 5;
+		updateDisplayF();
 	};
 	
 	
-	
+	updateDisplayF();
 	initB.addEventListener("click", initilizeF, false);
-	initB.addEventListener("click", makeCoffeeF, false);
+	makeB.addEventListener("click", makeCoffeeF, false);
 	//inx1.addListener("onchange",updateDispayF, false);
 	return 0;
 };	
